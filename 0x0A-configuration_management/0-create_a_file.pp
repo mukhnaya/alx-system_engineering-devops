2deafbfd77f9ca132361577f6b1436f639389e1c
mukhnaya <mosesmukhanya@gmail.com>
2023-06-25 00:33:12 +0000

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # to create a file in /temp directory
 file {'school':
   path    => '/tmp/school',
-  mode    => '0744'
+  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  mode    => '0744'
 }
